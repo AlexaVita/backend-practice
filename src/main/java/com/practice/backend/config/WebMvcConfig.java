6f202c66bf24c9f5c399898163224517697cb4de
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2023-06-28 10:06:24 +0300

    loging into console and requestUUID added

---
+++ src/main/java/com/practice/backend/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.practice.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final RequestUUIDInterceptor interceptor;
+
+    public WebMvcConfig(RequestUUIDInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+
+}
