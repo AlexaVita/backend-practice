85a89a828b02211dc276c607e24e311922e4dbd5
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2023-06-28 14:50:53 +0300

    after success logging added and after throwing logging added

--- src/main/java/com/practice/backend/aop/LoggingAspects.java
+++ src/main/java/com/practice/backend/aop/LoggingAspects.java
@@ -2,8 +2,7 @@ package com.practice.backend.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
@@ -28,4 +27,25 @@ public class LoggingAspects {
         log.trace(startMessage);
     }
 
+    @AfterReturning("Pointcuts.allMainControllerMethodsWithUUID()")
+    public void afterAllMainControllerMethodsWithUUID(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Object[] arguments = joinPoint.getArgs();
+
+        String startMessage = MessageFormat.format("Request named ''{0}'' with ID {1} has been finished with success",
+                methodSignature.getName(), arguments[0].toString());
+
+        log.trace(startMessage);
+    }
+
+    @AfterThrowing(pointcut = "Pointcuts.allMainControllerMethodsWithUUID()", throwing = "e")
+    public void afterThrowingInAllMainControllerMethodsWithUUID(JoinPoint joinPoint, Exception e) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Object[] arguments = joinPoint.getArgs();
+
+        String startMessage = MessageFormat.format("Request named ''{0}'' with ID {1} has been failed with error: {2}",
+                methodSignature.getName(), arguments[0].toString(), e.getMessage());
+
+        log.error(startMessage);
+    }
 }
