eb8fb66fb32212d9725d4cf8fc1b8fe374b3a1c9
Vita <0alexa.vita0@gmail.com>
2023-07-02 10:07:59 +0300

    example of mybatis

--- src/main/java/com/practice/backend/controller/MainRestController.java
+++ src/main/java/com/practice/backend/controller/MainRestController.java
@@ -1,35 +1,40 @@
 package com.practice.backend.controller;
 
-import com.practice.backend.repo.ExampleRepo;
+import com.practice.backend.mapper.ExampleMapper;
+import com.practice.backend.model.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class MainRestController {
+
     @Autowired
-    ExampleRepo exampleRepo;
+    ExampleMapper exampleMapper;
+//    @Autowired
+//    ExampleRepo exampleRepo;
 
     @GetMapping("/ping")
     public String ping(@RequestAttribute("requestUUID") UUID requestUUID) {
         return "pong";
     }
 
+//    @GetMapping("/numbers")
+//    public List<String> getNumbers() {
+//        try {
+//            return exampleRepo.getNumbers();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return Stream.of("SQL exception was thrown").collect(Collectors.toList());
+//        }
+//    }
+
     @GetMapping("/numbers")
-    public List<String> getNumbers() {
-        try {
-            return exampleRepo.getNumbers();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return Stream.of("SQL exception was thrown").collect(Collectors.toList());
-        }
+    public List<Example> getNumbers() {
+        return exampleMapper.getAll();
     }
 }
---
+++ src/main/java/com/practice/backend/mapper/ExampleMapper.java
@@ -0,0 +1,14 @@
+package com.practice.backend.mapper;
+
+import com.practice.backend.model.Example;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ExampleMapper {
+    //@Select("select * from example")
+    List<Example> getAll();
+
+}
+
