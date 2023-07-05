99cacc23b075b1b62dfa500b1aedb2a3fbdc2c24
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2023-07-06 00:19:35 +0300

    Update PaymentUtil.java
    Добавил проверку карты на отсутствие символов
    Добавил метод checkIp
    Добавил метод getPanMask

--- src/main/java/com/practice/backend/util/PaymentUtil.java
+++ src/main/java/com/practice/backend/util/PaymentUtil.java
@@ -12,10 +12,11 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public class PaymentUtil {
 
-    public static void checkSignature(HttpServletRequest request, Sector paymentSector, List<String> paymentParamsNames) throws PaymentException {
+    public static void checkSignature(HttpServletRequest request, UUID userUUID, Sector paymentSector, List<String> paymentParamsNames) throws PaymentException {
         // Берём сигнатуру из реквест-параметра
         String signature = (String) request.getAttribute("signature");
 
@@ -38,9 +39,6 @@ public class PaymentUtil {
 
         String countedSignature;
 
-        // Для отслеживания ошибок
-        UUID userUUID = (UUID) request.getAttribute("userUUID");
-
         try {
             // Результат - конвертация в строку через Base64 результата конвертации начальной строки через SHA256 в массив байтов
             countedSignature = convertThroughBase64(convertThroughSHA256(signatureBuilder.toString()));
@@ -55,9 +53,16 @@ public class PaymentUtil {
     }
 
     public static void checkPan(UUID uuid, String pan) throws PaymentException {
-        //превращаем строку в массив чисел
-        String[] strArr = pan.split("");
-        int[] digits = Arrays.stream(strArr).mapToInt(Integer::parseInt).toArray();
+        //превращаем строку в массив чисел + проверка чтобы все были цифрами
+        if (!pan.matches("\\d+")) {
+            throw new PaymentException(PaymentExceptionCodes.INVALID_CARD, uuid, "Неверно заполнена карта");
+        }
+
+        int[] digits = pan.chars().
+                mapToObj(c -> (char) c)
+                .mapToInt(Character::getNumericValue)
+                .toArray();
+
         //проверка на бизнес-требования
         if (digits.length < 16 || digits.length > 19 || (digits[0] != 2 && digits[0] != 4 && digits[0] != 5))
             throw new PaymentException(PaymentExceptionCodes.INVALID_CARD, uuid, "Неверно заполнена карта");
@@ -77,6 +82,25 @@ public class PaymentUtil {
             throw new PaymentException(PaymentExceptionCodes.INVALID_CARD, uuid, "Неверно заполнена карта");
         }
 
+    }
+
+    public static void checkIp(HttpServletRequest request, UUID userUUID, Sector paymentSector) throws PaymentException {
+        if (paymentSector.getCheckIp()) {
+            String allowedIps = paymentSector.getAllowedIps();
+            // Если IP из реквеста не содержится в строке allowedIps экземпляра класса Sector, то кидаем "абстрактную" ошибку
+            if (!allowedIps.contains(request.getRemoteAddr())) {
+                throw new PaymentException(PaymentExceptionCodes.INTERNAL_ERROR, userUUID, "Внутренняя ошибка");
+            }
+        }
+    }
+
+    public static String getPanMask(String pan) {
+        // По правилу 6 в начале, 4 в конце
+        String mask = "*";
+        int start = 6;
+        int end = pan.length() - 4;
+        // Итоговая строка - первые 6 символов + маска (любые символы ["."] между на *) + последние 4 символа
+        return pan.substring(0, start) + pan.substring(start, end).replaceAll(".", mask) + pan.substring(end);
     }
 
     private static byte[] convertThroughSHA256(String initialString) throws NoSuchAlgorithmException {
